d746b632d3df1ed601bc3d1cd9d78ab3e2faad09
JurajMarincic <juraj.marincic1@gmail.com>
2023-06-06 21:35:40 +0200

    Add project files.

---
+++ RSSV LV1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace RSSV_LV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+     
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.hourBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.minutesBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(446, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // hourBox
+            // 
+            this.hourBox.Location = new System.Drawing.Point(260, 155);
+            this.hourBox.Name = "hourBox";
+            this.hourBox.Size = new System.Drawing.Size(115, 20);
+            this.hourBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(381, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hours";
+
+            // 
+            // minutesBox
+            // 
+            this.minutesBox.Location = new System.Drawing.Point(260, 193);
+            this.minutesBox.Name = "minutesBox";
+            this.minutesBox.Size = new System.Drawing.Size(115, 20);
+            this.minutesBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(381, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Minutes";
+
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(305, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "21:12:58";
+
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Postavi alarm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.minutesBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hourBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox hourBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox minutesBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RSSV LV1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSSV_LV1
+{
+    public partial class Form1 : Form
+    {
+
+
+        private string hours, minutes;
+        private bool alarmSet;
+        private System.Timers.Timer t;
+        public Form1()
+        {
+            InitializeComponent();
+            //Kreiranje timer-a s periodom od 1000 ms
+            t = new System.Timers.Timer(1000);
+            //Dodavanje događaja timer-u; izvršava se periodno
+            t.Elapsed += new System.Timers.ElapsedEventHandler(vrijeme);
+            t.Start();
+        }
+
+
+
+        private void vrijeme(object s, EventArgs e)
+        {
+            //Metoda Invoke izvršava delegata na niti koja je vlasnik
+            //rukovatelja kontrola (uobičajeno, glavna nit)
+            //MethodInvoker je delegat koji može izvršiti bilo koju
+            //metodu koja ne vraća ništa i nema parametre
+            Invoke((MethodInvoker)delegate //Anonimna metoda
+            {
+                label4.Text = DateTime.Now.ToLongTimeString();
+               
+
+                if (hours == DateTime.Now.Hour.ToString() && minutes == DateTime.Now.Minute.ToString() && alarmSet)
+                    {
+                        callAlarm();
+                    }
+
+                
+            });
+        }
+
+
+        private void button1_Click(object sender,
+EventArgs e)
+        {
+ 
+
+           if (hourBox.Text == "" || minutesBox.Text=="")
+            {
+                MessageBox.Show("Unesite sate i minute!");
+            }
+           else
+            {
+                hours = hourBox.Text;
+                minutes = minutesBox.Text;
+                alarmSet = true;
+            }
+
+           
+        }
+
+
+        private void callAlarm()
+        {
+            Console.Beep(3000, 1000);
+            alarmSet = false;
+            MessageBox.Show("Alarm!!!!");
+        }
+
+
+    
+    }
+}
